e6c2538f292dec33dd3dcb24117071f8a27bd4bc
Christoph Raidl <37546897+darkwheel@users.noreply.github.com>
2019-09-25 18:57:21 +0200

    Update CardCreator.py

--- CardCreator.py
+++ CardCreator.py
@@ -2,24 +2,24 @@ import pprint
 from flask import send_file
 
 def CardCreator(company, phoneNumber, street, city, region, code, country, website, note):
-    file = open("tmp/" + str(company) + ".vcf", "w")
+    file = tempfile.TemporaryFile("w+b")
 
-    file.write("BEGIN:VCARD\nVERSION:3.0\nPRODID:-//Apple Inc.//iPhone OS 13.0//EN\nN:;;;;\n")
-    file.write("FN:" + company + "\n")
-    file.write("ORG:" + company + "\n")
+    file.write("BEGIN:VCARD\nVERSION:3.0\nPRODID:-//Apple Inc.//iPhone OS 13.0//EN\nN:;;;;\n".encode("utf-8"))
+    file.write(("FN:" + str(company) + "\n").encode("utf-8"))
+    file.write(("ORG:" + str(company) + "\n").encode("utf-8"))
     if note:
-        file.write("NOTE:")
+        file.write("NOTE:".encode("utf-8"))
         for i in range(0, len(note)):
             if i is len(note)-1:
-                file.write(note[i])
+                file.write(str(note[i]).encode("utf-8"))
                 continue
-            file.write(note[i] + "\\n")
+            file.write((str(note[i]) + "\\n").encode("utf-8"))
 
-    file.write("\nTEL;type=WORK;type=VOICE;type=pref:" + phoneNumber + "\n")
-    file.write("item1.ADR;type=WORK;type=pref:;;" + street + ";" + city + ";" + region + ";" + code + ";" + country + "\n")
-    file.write("item1.X-ABADR:de\n")
-    file.write("item2.URL;type=pref:" + website + "\n")
-    file.write("item2.X-ABLabel:_$!<HomePage>!$_\nX-ABShowAs:COMPANY\nEND:VCARD\n")
-
-    file.close()
-    send_file("../tmp/Phi Asia.vcf")
+    file.write(("\nTEL;type=WORK;type=VOICE;type=pref:" + str(phoneNumber) + "\n").encode("utf-8"))
+    file.write(("item1.ADR;type=WORK;type=pref:;;" + str(street) + ";" + str(city) + ";" + str(region) + ";" + str(code) + ";" + str(country) + "\n").encode("utf-8"))
+    file.write("item1.X-ABADR:de\n".encode("utf-8"))
+    file.write(("item2.URL;type=pref:" + str(website) + "\n").encode("utf-8"))
+    file.write("item2.X-ABLabel:_$!<HomePage>!$_\nX-ABShowAs:COMPANY\nEND:VCARD\n".encode("utf-8"))
+    
+    S3 = tinys3.Connection(access_key,secret_key,tls=True)
+    S3.upload('Phi Asia.vcf',file,'contactcreator')
