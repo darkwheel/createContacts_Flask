0a9419695378f501d3c0cdc36512e01d8e383c1d
Christoph Raidl <37546897+darkwheel@users.noreply.github.com>
2019-09-26 14:31:40 +0200

    Update CardCreator.py

--- CardCreator.py
+++ CardCreator.py
@@ -1,8 +1,15 @@
-import pprint
-from flask import send_file
-import tinys3
+import tempfile
+import boto3
+import botocore
+import urllib
+from botocore.client import Config
+import os
 
 def CardCreator(company, phoneNumber, street, city, region, code, country, website, note):
+    access_key = os.environ['S3_KEY']
+    secret_key = os.environ['S3_SECRET']
+    key = str(company) + '.vcf'
+    bucket = os.environ['S3_BUCKET']
     file = tempfile.TemporaryFile("w+b")
 
     file.write("BEGIN:VCARD\nVERSION:3.0\nPRODID:-//Apple Inc.//iPhone OS 13.0//EN\nN:;;;;\n".encode("utf-8"))
@@ -21,6 +28,11 @@ def CardCreator(company, phoneNumber, street, city, region, code, country, websi
     file.write("item1.X-ABADR:de\n".encode("utf-8"))
     file.write(("item2.URL;type=pref:" + str(website) + "\n").encode("utf-8"))
     file.write("item2.X-ABLabel:_$!<HomePage>!$_\nX-ABShowAs:COMPANY\nEND:VCARD\n".encode("utf-8"))
-    
-    S3 = tinys3.Connection(AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY,tls=True)
-    S3.upload(str(company) + '.vcf',file,S3_BUCKET)
+
+    file.seek(0)
+    s3_client = boto3.client('s3', region_name=regionS3, aws_access_key_id=access_key, aws_secret_access_key=secret_key, config=Config(signature_version='s3v4'))
+    s3 = boto3.resource('s3', region_name=regionS3, aws_access_key_id=access_key, aws_secret_access_key=secret_key, config=Config(signature_version='s3v4'))
+
+    s3.Bucket(bucket).put_object(Key=key, Body=file)
+    objectUrl = s3_client.generate_presigned_url(ClientMethod='get_object',Params={'Bucket': bucket,'Key': key})
+    urllib.request.urlretrieve(str(objectUrl))
