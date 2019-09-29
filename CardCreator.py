7d5ac9e04c687fdd4646dad2129cbee4ee6f7a9b
Christoph Raidl <christoph@raidl.de>
2019-09-29 22:24:05 +0200

    save in specific S3 folder

--- CardCreator.py
+++ CardCreator.py
@@ -10,7 +10,7 @@ def CardCreator(company, phoneNumber, street, city, region, code, country, websi
     access_key = os.environ['S3_KEY']
     secret_key = os.environ['S3_SECRET']
     regionS3 = "eu-central-1"
-    key = str(company) + '.vcf'
+    key = "contactcards/" + str(company) + '.vcf'
     bucket = os.environ['S3_BUCKET']
     file = tempfile.TemporaryFile("w+b")
 
@@ -36,7 +36,7 @@ def CardCreator(company, phoneNumber, street, city, region, code, country, websi
     s3 = boto3.resource('s3', region_name=regionS3, aws_access_key_id=access_key, aws_secret_access_key=secret_key, config=Config(signature_version='s3v4'))
 
     s3.Bucket(bucket).put_object(Key=key, Body=file)
-    objectUrl = s3_client.generate_presigned_url(ClientMethod='get_object',Params={'Bucket': bucket,'Key': key})
+    objectUrl = s3_client.generate_presigned_url(ClientMethod='get_object', Params={'Bucket': bucket, 'Key': key})
     response = urllib.request.urlretrieve(str(objectUrl))
     print(response)
     
